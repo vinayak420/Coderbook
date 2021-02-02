9314ad35cbe32c7da3022837d9ad226c3706b278
Vinayak Mishra <vinayakmishra98@yahoo.com>
2021-02-02 10:53:16 +0530

    Installed Dependencies and set up database

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./config/db")
 
 app.get("/", (req, res)=>{
     res.send("API is Running")
 })
 
+connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>{
